dca15caa2cfe17dbe87835915f6f7fd993f553bf
Mickael VIEGAS <mviegas@excilys.com>
2016-12-07 12:28:58 +0100

    [#2] Récupération de l'évènement quand on est connecté à internet ou pas

--- app/src/main/java/com/excilys/mviegas/computer_database/android/services/impl/InternetHelperImpl.java
+++ app/src/main/java/com/excilys/mviegas/computer_database/android/services/impl/InternetHelperImpl.java
@@ -1,8 +1,12 @@
 package com.excilys.mviegas.computer_database.android.services.impl;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 
 import com.excilys.mviegas.computer_database.android.services.InternetService;
 
@@ -12,14 +16,34 @@ import com.excilys.mviegas.computer_database.android.services.InternetService;
  * @author VIEGAS Mickael
  */
 public class InternetHelperImpl implements InternetService {
+    private static final String TAG = "InternetHelperImpl";
+
     private Context context;
     private NetworkInfo networkInfo;
 
     public InternetHelperImpl(Context context) {
         this.context = context;
 
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        final ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         this.networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        IntentFilter intentFilterConnectivity = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
+        context.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(TAG, "onReceive() called with: context = [" + context + "], intent = [" + intent + "]");
+
+                if (intent.getExtras() != null) {
+                    // On récupère à chaque fois le réseau actif car il change si on change de réseau
+                    networkInfo = connectivityManager.getActiveNetworkInfo();
+                    if (networkInfo != null && networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                        Log.i(TAG, "Network " + networkInfo.getTypeName() + " connected");
+                    } else if (intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, Boolean.TRUE)) {
+                        Log.d(TAG, "There's no network connectivity");
+                    }
+                }
+            }
+        }, intentFilterConnectivity);
     }
 
     @Override
